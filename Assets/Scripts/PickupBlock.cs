fe1210f78e07cf2ced2eb561df8659282d6798f1
cmbak <chikmii@gmail.com>
2024-10-10 11:35:45 +0100

    Make block go to ground when dropped

--- Assets/Scripts/PickupBlock.cs
+++ Assets/Scripts/PickupBlock.cs
@@ -7,11 +7,23 @@ public class PickupBlock : MonoBehaviour
     public Rigidbody boxRb;
     public float pickUpRange;
     private bool isHoldingItem = false;
+    private const float ADD_GROUND_Y = 0.26F; // To stop box from ending up halfway in the ground
 
     // Start is called before the first frame update
     void Start()
     {
         boxRb.isKinematic = true; // So block doesn't move
+    }
+
+    // Returns position of ground or of transform if no ground 
+    Vector3 getGroundPos()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hitInfo, Mathf.Infinity, LayerMask.GetMask("Ground")))
+        {
+            return hitInfo.point;
+        }
+        return transform.position;
     }
 
     // Update is called once per frame
@@ -31,7 +43,12 @@ public class PickupBlock : MonoBehaviour
         else if (isHoldingItem && Input.GetKeyDown(KeyCode.G))
         {
             isHoldingItem = false;
+
+            // Make object go on ground
+            Vector3 groundPos = getGroundPos();
             transform.SetParent(null);
+            transform.SetPositionAndRotation(new Vector3(transform.position.x, groundPos.y + ADD_GROUND_Y, transform.position.z), Quaternion.identity);
+
             Debug.Log("Player has dropped their item");
         }
     }
