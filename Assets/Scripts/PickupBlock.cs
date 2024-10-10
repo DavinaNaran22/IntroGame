73495f1c2e08cf0728e0dc7a96abf44a14fdf35b
cmbak <chikmii@gmail.com>
2024-10-10 12:38:12 +0100

    Make boxes placeable on specific area

--- Assets/Scripts/PickupBlock.cs
+++ Assets/Scripts/PickupBlock.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PickupBlock : MonoBehaviour
@@ -7,7 +8,9 @@ public class PickupBlock : MonoBehaviour
     public Rigidbody boxRb;
     public float pickUpRange;
     private bool isHoldingItem = false;
+    private bool canBePicked = true;
     private const float ADD_GROUND_Y = 0.26F; // To stop box from ending up halfway in the ground
+    private int collected = 0; // Needs to be somewhere else
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +29,28 @@ public class PickupBlock : MonoBehaviour
         return transform.position;
     }
 
+    // Returns layer below block as int
+    int GetLayerBelow()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hitInfo, Mathf.Infinity))
+        {
+            return hitInfo.transform.gameObject.layer;
+        }
+        return transform.gameObject.layer;
+    }
+
     // Update is called once per frame
     void Update()
     {
         Vector3 distanceToPlayer = player.position - transform.position;
         // If player isn't holding anything and presses grab button
-        if (!isHoldingItem && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.G)) // https://www.youtube.com/watch?v=8kKLUsn7tcg
+        if (!isHoldingItem && canBePicked && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.G)) // https://www.youtube.com/watch?v=8kKLUsn7tcg
         {
             isHoldingItem = true;
             transform.SetParent(boxContainer);
             transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity); // box moved to same position as parent (boxContainer) w/o rotating it
+
             Debug.Log("Player is holding an item");
         }
 
@@ -43,12 +58,18 @@ public class PickupBlock : MonoBehaviour
         else if (isHoldingItem && Input.GetKeyDown(KeyCode.G))
         {
             isHoldingItem = false;
-
-            // Make object go on ground
-            Vector3 groundPos = getGroundPos();
             transform.SetParent(null);
-            transform.SetPositionAndRotation(new Vector3(transform.position.x, groundPos.y + ADD_GROUND_Y, transform.position.z), Quaternion.identity);
 
+            // If CanBePlacedOn is below, stop it from being grabbed
+            if (GetLayerBelow() == LayerMask.NameToLayer("CanBePlacedOn"))
+            {
+                collected++;
+                Debug.Log("Block has been placed on correct spot! " + collected + "/4");
+                canBePicked = false;
+            }
+
+            Vector3 groundPos = getGroundPos();
+            transform.SetPositionAndRotation(new Vector3(transform.position.x, groundPos.y + ADD_GROUND_Y, transform.position.z), Quaternion.identity);
             Debug.Log("Player has dropped their item");
         }
     }
