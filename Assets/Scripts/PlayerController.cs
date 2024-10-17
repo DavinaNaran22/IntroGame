ea3d5ebb8eae782438b5c107a50b8778bab95b9b
cmbak <chikmii@gmail.com>
2024-10-17 11:50:10 +0100

    Move files to folders and add interior to landscape scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private int boxesCollected;
     public UnityEvent collectBoxEvent = new UnityEvent();
-    public TextMeshProUGUI repairText;
+    //public TextMeshProUGUI repairText; don't want to use external assets :)
     public int testing;
 
     private void Start()
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     public void CollectBox()
     {
         boxesCollected++;
-        repairText.text = "Spaceship Repairs: " + boxesCollected + "/4";
+        //repairText.text = "Spaceship Repairs: " + boxesCollected + "/4";
         Debug.Log("Boxes collected " + boxesCollected); // https://www.youtube.com/watch?v=djW7g6Bnyrc
     }
 
