53986208e5161b6ca4a82f708d19733698abb545
cmbak <chikmii@gmail.com>
2024-11-15 21:22:15 +0000

    Change scenes using async additive scenes

--- Assets/Scripts/CaveTransition.cs
+++ Assets/Scripts/CaveTransition.cs
@@ -6,12 +6,36 @@ using UnityEngine.SceneManagement;
 public class CaveTransition: MonoBehaviour
 {
     public string scene;
+    public Vector3 spawnPos;
     //when player enters cave switches scene
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(scene);
+            StartCoroutine(LoadAsyncScene());
+            other.transform.position = spawnPos;
         }
+    }
+
+    IEnumerator LoadAsyncScene()
+    {
+        Scene currScene = SceneManager.GetActiveScene();
+
+        // Load new scene at the same time as current scene
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move player and ui to new scene
+        SceneManager.MoveGameObjectToScene(GameObject.FindWithTag("Player"), SceneManager.GetSceneByName(scene));
+        SceneManager.MoveGameObjectToScene(GameObject.FindWithTag("UIManager"), SceneManager.GetSceneByName(scene));
+
+        //currScene.GetRootGameObjects.
+
+        // Unload old scene
+        SceneManager.UnloadSceneAsync(currScene);
     }
 }
