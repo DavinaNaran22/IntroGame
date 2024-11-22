b1040a8122e38ab1cd82f86e4c8f85aebc04d757
cmbak <chikmii@gmail.com>
2024-11-22 15:42:45 +0000

    Add cockpit to interior

---
+++ Assets/Scripts/TeleportCockpit.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportCockpit : MonoBehaviour
+{
+
+    // If player is near door
+    //      on trigger enter?
+    // And presses e?
+    // Then teleport player to cockpit
+    // Can't move body, can only rotate camera
+
+    // Can press escape to exit cockpit
+    // Should go back to the outside of door
+
+    // Bonus - show text when near
+
+    [SerializeField] private Vector3 chairCoords;
+    //[SerializeField] private Vector3 corridorCoords;
+    // Have script which teleports player?
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
