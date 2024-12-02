ead9a6a3455baf825db86b1548af679050302973
DavinaNaran22 <157966175+DavinaNaran22@users.noreply.github.com>
2024-12-02 14:53:41 +0000

    Hides HUD elements until after cutscene finishes

--- Assets/Scripts/CutsceneController.cs
+++ Assets/Scripts/CutsceneController.cs
@@ -6,10 +6,31 @@ using Unity.Cinemachine;
 public class CutsceneController : MonoBehaviour
 {
     public CinemachineThirdPersonAim cameraFollow;
+    public GameObject HUD;
+    public GameObject progressButton;
+    public GameObject inventoryManager;
+    public GameObject inventoryCanvas;
+    public GameObject hovertext;
+    public GameObject minimapCanvas;
+    
 
-    //Disable cinemachine camera so player can get control back
+    // Disable cinemachine camera so player can get control back
+    // Enable progress button and hovertext on screen
     public void EndCamera()
     {
         cameraFollow.gameObject.SetActive(false);
+        progressButton.SetActive(true);
+        hovertext.SetActive(true);
+    }
+
+    // Hide all non-cutscene elements
+    public void stopHUD()
+    {
+        HUD.SetActive(false);
+        inventoryManager.SetActive(false);
+        progressButton.SetActive(false);
+        inventoryCanvas.SetActive(false);
+        hovertext.SetActive(false);
+        minimapCanvas.SetActive(false);
     }
 }
