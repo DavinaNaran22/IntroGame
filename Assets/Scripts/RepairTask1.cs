4d9c2d9120353d023125319bc787330746d524ce
DavinaNaran22 <157966175+DavinaNaran22@users.noreply.github.com>
2024-12-13 20:53:37 +0000

    DON'T COPY ENTIRE SCRIPT BC IT'S NOT UPDATED WITH CAMERA
    Just add the alien stuff

--- Assets/Scripts/RepairTask1.cs
+++ Assets/Scripts/RepairTask1.cs
@@ -1,21 +1,32 @@
-using UnityEngine;
+using System.Collections.Generic;
 using TMPro;
-using UnityEngine.InputSystem;
+using UnityEngine;
 
 public class MissionManager : MonoBehaviour
 {
-    public GameObject player; 
-    public BoxCollider restrictedArea; 
-    public TextMeshProUGUI promptText; 
+    public GameObject player;
+    public BoxCollider restrictedArea;
+    public TextMeshProUGUI promptText;
     public TextMeshProUGUI dialogueText;
 
-    private bool photoTaken = false; 
-    private bool dialogueShown = false; 
+    private bool photoTaken = false;
+    private bool dialogueShown = false;
+    private bool additionalDialoguesActive = false;
+    private int currentDialogueIndex = 0;
+
+
     private Vector3 minBounds; // Minimum bounds of the restricted area
     private Vector3 maxBounds; // Maximum bounds of the restricted area
-    private CharacterController characterController; 
+    private CharacterController characterController;
     private PlayerInputActions inputActions;
 
+
+    private List<string> additionalDialogues = new List<string>
+    {
+        "Hello, my spaceship’s hit an asteroid, and this was the only nearby planet.",
+        "I really need that thruster so I can repair my ship and leave.",
+        "Can I quickly grab it? I won’t be long."
+    };
 
     // New input system for taking photos and dismissing dialogue
     private void Awake()
@@ -40,8 +51,7 @@ public class MissionManager : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log("GameManager");
-        Debug.Log(GameManager.Instance.player);
+
         player = GameManager.Instance.player;
 
         // Calculate the bounds of the restricted area
@@ -89,6 +99,11 @@ public class MissionManager : MonoBehaviour
         {
             HideDialogue();
             dialogueShown = true;
+        }
+
+        else if (additionalDialoguesActive)
+        {
+            ShowNextDialogue();
         }
     }
 
@@ -144,11 +159,34 @@ public class MissionManager : MonoBehaviour
         Debug.Log("Photo taken!");
         RemoveRestriction();
         HidePrompt(); // Hide the prompt after taking a photo
+        StartAdditionalDialogues();
     }
 
     private void RemoveRestriction()
     {
         // Remove the restriction logic
         restrictedArea.enabled = false;
+    }
+
+    private void StartAdditionalDialogues()
+    {
+        additionalDialoguesActive = true;
+        currentDialogueIndex = 0;
+        ShowDialogue(additionalDialogues[currentDialogueIndex]);
+    }
+
+    private void ShowNextDialogue()
+    {
+        currentDialogueIndex++;
+
+        if (currentDialogueIndex < additionalDialogues.Count)
+        {
+            ShowDialogue(additionalDialogues[currentDialogueIndex]);
+        }
+        else
+        {
+            additionalDialoguesActive = false;
+            HideDialogue();
+        }
     }
 }
