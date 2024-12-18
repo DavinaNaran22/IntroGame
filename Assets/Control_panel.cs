a18aa43516500f11c928f54bb42eba71cc2a5b2e
Rachel Wolde <rachelwolde25@gmail.com>
2024-12-18 01:20:56 +0000

    Task 1 base completed

---
+++ Assets/Control_panel.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class Control_panel : MonoBehaviour
+{
+    public Collider Cockpit;
+    public bool IN = false;
+    public GameObject C_panel;
+    public Canvas ship_status;
+    public Canvas ship_map;
+    public GameObject Message;
+    public GameObject Message2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Message.SetActive(false);
+        C_panel.SetActive(false);
+        Message2.SetActive(false);
+        ship_map.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"IN state: {IN}");
+
+        if (IN == true && Input.GetKeyDown(KeyCode.A)) {
+            Debug.Log("ACTIVE");
+             C_panel.SetActive(true);
+            ship_map.enabled = false;
+            Message.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            IN = true;
+            Message.SetActive(true);
+            Debug.Log("WERE IN");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            IN = false;
+            Message.SetActive(false);
+            C_panel.SetActive(false);
+            Message2.SetActive(true);
+            Debug.Log("WERE IN");
+        }
+    }
+
+    public void enable_ship_map() {
+        ship_map.enabled = true;
+        ship_status.enabled = false;
+
+    }
+    public void enable_ship_status()
+    {
+        ship_map.enabled = false;
+        ship_status.enabled = true;
+
+    }
+
+
+
+}
