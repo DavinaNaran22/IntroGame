74ea13dab033eaab4dd66673f5dc64870cc60ae7
DavinaNaran22 <157966175+DavinaNaran22@users.noreply.github.com>
2024-12-26 13:32:34 +0000

    Set up scene 4 wings and changed dialogue line in scene 3

---
+++ Assets/Scripts/RepairTask4.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using UnityEngine.XR;
+using Unity.VisualScripting;
+
+public class RepairTask4 : MonoBehaviour
+{
+    public GameObject player;
+    public TextMeshProUGUI dialogueText;
+
+
+    private bool dialogueShown = false;
+    private bool additionalDialoguesActive = false;
+    private int currentDialogueIndex = 0;
+
+
+    private PlayerInputActions inputActions;
+
+
+    public List<string> additionalDialogues = new List<string>
+    {
+        "Finally, I’m on the last task.",
+        "I need to find the wing that came off when we crashed.",
+        "It must be around the spaceship.",
+    };
+
+
+    // New input system for taking photos and dismissing dialogue
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.DismissDialogue.performed += ctx => DismissDialogue(); // Press D to dismiss dialogue
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Disable();
+        inputActions.Player.DismissDialogue.performed -= ctx => DismissDialogue();
+    }
+
+    private void Start()
+    {
+        Debug.Log("RepairTask4 Start");
+        player = GameManager.Instance.player;
+        StartAdditionalDialogues();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void DismissDialogue()
+    {
+        if (!additionalDialoguesActive) return;
+
+        if (additionalDialoguesActive)
+        {
+            ShowNextDialogue();
+        }
+        else
+        {
+            HideDialogue();
+            dialogueShown = true;
+        }
+    }
+
+    private void ShowDialogue(string message)
+    {
+        dialogueText.gameObject.SetActive(true);
+        dialogueText.text = message;
+    }
+
+    private void HideDialogue()
+    {
+        dialogueText.gameObject.SetActive(false);
+    }
+
+    // Dialogue between player
+    public void StartAdditionalDialogues()
+    {
+        Debug.Log("Starting additional dialogues");
+        additionalDialoguesActive = true;
+        currentDialogueIndex = 0;
+        ShowDialogue(additionalDialogues[currentDialogueIndex]);
+    }
+
+    // Display the next dialogue in the list
+    private void ShowNextDialogue()
+    {
+        currentDialogueIndex++;
+
+        if (currentDialogueIndex < additionalDialogues.Count)
+        {
+            ShowDialogue(additionalDialogues[currentDialogueIndex]);
+        }
+        else
+        {
+            additionalDialoguesActive = false;
+            HideDialogue();
+
+            Debug.Log("All dialogues finished");
+
+        }
+    }
+
+    public bool IsDialogueActive()
+    {
+        //return dialogueText.gameObject.activeSelf;
+        return additionalDialoguesActive;
+    }
+}
+
