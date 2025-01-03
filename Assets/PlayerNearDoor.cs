44522cc9d9cac5574ccd0378595fc3b4bfee76cd
cmbak <chikmii@gmail.com>
2025-01-03 11:00:06 +0000

    Fix interior hover text

---
+++ Assets/PlayerNearDoor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNearDoor : PlayerNearText
+{
+
+    new void Update()
+    {
+        base.Update();
+        if (GameManager.Instance.unlockedDoor)
+        {
+            // Prevents hover text from briefly flickering to 'Right click to open keypad'
+            // Upon the first time player right clicks door
+            Text = "Right click to leave ship";
+        }
+    }
+}
