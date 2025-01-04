f07820aebbd07ac8cf39741a06e484d66ddb6154
cmbak <chikmii@gmail.com>
2025-01-04 21:12:00 +0000

    Add death and win scenes to build

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -132,7 +132,8 @@ public class PlayerHealth : MonoBehaviour
         if (!hasDied)
         {
             hasDied = true;
-            StartCoroutine(LoadDeathSceneAsync());
+            SceneManager.LoadScene("Death");
+            //StartCoroutine(LoadDeathSceneAsync());
         }
     }
 
